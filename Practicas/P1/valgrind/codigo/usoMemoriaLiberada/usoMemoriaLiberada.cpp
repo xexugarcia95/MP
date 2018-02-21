418b307d1f43ab76c74a1e6a9cfc6f2061cd3899
Jesus Garcia <xexugarcia95@gmail.com>
2018-02-21 18:57:46 +0100

    Valgrind

---
+++ Practicas/P1/valgrind/codigo/usoMemoriaLiberada/usoMemoriaLiberada.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    // Se reserva espacio para p
+    char *p = new char;
+    
+    // Se da valor
+    *p = 'a'; 
+
+    // Se copia el caracter en c
+    char c = *p; 
+
+    // Se muestra
+    cout << "Caracter c: " << c; 
+
+    // Se libera el espacio
+    delete p;
+    
+    // Se copia el contenido de p (YA LIBERADO) en c
+    c = *p;
+    return 0;
+}
