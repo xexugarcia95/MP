2051ca4e6452edadb58dbf8e3040896778a3aad8
xexugarcia95 <xexugarcia95@gmail.com>
2018-03-14 17:09:34 +0100

    Añadidos elementos del ejercicio2. Preparado para implementar utilidades

--- Practicas/P2/ejercicio2/include/Valor.h
+++ Practicas/P2/ejercicio2/include/Valor.h
@@ -1,4 +1,3 @@
-
 #ifndef VALOR_H
 #define VALOR_H
 
---
+++ Practicas/P2/ejercicio2/include/utilidades.h
@@ -0,0 +1,12 @@
+#ifndef _UTILIDADES_H__
+#define _UTILIDADES_H__
+
+#include <iostream>
+#include "Valor.h"
+
+using namespace std;
+
+
+
+
+#endif
