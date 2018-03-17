a636245488a39cd1c64c4d06096bd4dd83674b18
xexugarcia95 <xexugarcia95@gmail.com>
2018-03-17 13:21:56 +0100

    Implementación realizada, corregir funcion 'insertarCadena'

--- Practicas/P2/ejercicio5/include/utilidades.h
+++ Practicas/P2/ejercicio5/include/utilidades.h
@@ -1,7 +1,13 @@
 #ifndef _UTILIDADES_H__
 #define _UTILIDADES_H__
 
+#include <iostream>
 
+using namespace std;
+
+bool insertarCadena(char cadena1[],char cadena2[],int posicion,char resultado[]);
+
+int Longitud(char cadena[]); //Devuelve el tamaño de la cadena
 
 
 #endif
--- Practicas/P2/ejercicio5/src/utilidades.cpp
+++ Practicas/P2/ejercicio5/src/utilidades.cpp
@@ -1,1 +1,30 @@
 #include "utilidades.h"
+
+int Longitud(char cadena[])
+{
+  int cont=0;
+  for(int i=0;cadena[i]!='\0';i++) cont++;
+
+  return cont;
+}
+
+bool insertarCadena(char cadena1[],char cadena2[],int posicion,char resultado[])
+{
+  int tam = Longitud(cadena1);
+  bool insercion = false;
+  if(posicion<=tam)
+  {
+    insercion = true;
+    int tam2 = Longitud(cadena2);
+    for(int i=tam-1;i>=posicion;i--)
+    {
+      resultado[i+tam2] = cadena1[i];
+    }
+    for(int i=0;i<tam2;i++)
+    {
+      resultado[i] = cadena2[i];
+    }
+  }
+
+  return insercion;
+}
