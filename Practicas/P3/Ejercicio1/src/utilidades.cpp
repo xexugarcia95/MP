982cd8694aff1b9787f83e4cbd9ab81364669172
xexugarcia95 <xexugarcia95@gmail.com>
2018-04-10 17:10:02 +0200

    make y funcion imprimir con *, falta ordenar puntos e imprimir con **.

--- Practicas/P3/Ejercicio1/src/utilidades.cpp
+++ Practicas/P3/Ejercicio1/src/utilidades.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 void imprimir(int *secuencia, int util)
 {
-
+  for(int i=0;i<util;i++)
+  {
+    cout << secuencia[i] << " ";
+  }
 }
 
 void imprimir(int **secuencia, int util)
 {
-
+  
 }
 
 void ordenarPunteros(int *array, int **parray, int util)
 {
-  
+
 }
