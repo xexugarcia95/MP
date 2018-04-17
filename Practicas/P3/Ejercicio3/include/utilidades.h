1211f165ba9e9e3ba586c65fa13c85809683271e
xexugarcia95 <xexugarcia95@gmail.com>
2018-04-17 21:12:02 +0200

    Practica 3 Ejercicio 3

--- Practicas/P3/Ejercicio3/include/utilidades.h
+++ Practicas/P3/Ejercicio3/include/utilidades.h
@@ -1,8 +1,11 @@
 #ifndef _UTILIDADES_H__
 #define _UTILIDADES_H__
 
-void imprimirArray(int const * array,int util);
+#include <iostream>
+using namespace std;
 
-int mezclarUnico(int const * array1,int util1,int const * array2,int util2,int * array3);
+void imprimirArray(double const * array,int util);
+
+int mezclarUnico(double const * array1,int util1,double const * array2,int util2,double * array3);
 
 #endif
--- Practicas/P3/Ejercicio3/src/utilidades.cpp
+++ Practicas/P3/Ejercicio3/src/utilidades.cpp
@@ -1,6 +1,6 @@
 #include "utilidades.h"
 
-void imprimirArray(int const * array,int util)
+void imprimirArray(double const * array,int util)
 {
   for(int i=0;i<util;i++)
   {
@@ -9,7 +9,52 @@ void imprimirArray(int const * array,int util)
   cout << endl;
 }
 
-int mezclarUnico(int const * array1,int util1,int const * array2,int util2,int * array3)
+int mezclarUnico(const double * array1,int util1,const double * array2,int util2,double * array3)
 {
+  int util = util1+util2;
+  for(int i=0,j=0,k=0;k<util;k++)
+  {
 
+    if(array1[i] < array2[j])
+    {
+      if(i<util1)
+      {
+        array3[k] = array1[i];
+        i++;
+      }else if(i==util1)
+      {
+        array3[k] = array2[j];
+        j++;
+      }
+
+    }else if(array2[j] < array1[i])
+    {
+      if(j<util2)
+      {
+        array3[k] = array2[j];
+        j++;
+      }else if(j==util2)
+      {
+        array3[k] = array1[i];
+        i++;
+      }
+
+    }else
+    {
+      if(array1[i] == array2[j])
+      {
+        array3[k] = array2[j];
+        util--;
+        i++;
+        j++;
+      }
+
+    }
+
+  }
+
+  return util;
 }
+
+/*Otra manera de implementarlo seria añadir secuencialmente los dos vectores en el tercero,
+y posteriormente ordenarlos*/
