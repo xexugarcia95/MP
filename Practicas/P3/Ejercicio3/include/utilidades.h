de4f7ed54e7126d3309ec6c4cca81904ba491cbf
xexugarcia95 <xexugarcia95@gmail.com>
2018-04-17 14:10:06 +0200

    Implementar función mezclarUnico

--- Practicas/P3/Ejercicio3/include/utilidades.h
+++ Practicas/P3/Ejercicio3/include/utilidades.h
@@ -1,6 +1,8 @@
 #ifndef _UTILIDADES_H__
 #define _UTILIDADES_H__
 
+void imprimirArray(int const * array,int util);
 
+int mezclarUnico(int const * array1,int util1,int const * array2,int util2,int * array3);
 
 #endif
--- Practicas/P3/Ejercicio3/src/utilidades.cpp
+++ Practicas/P3/Ejercicio3/src/utilidades.cpp
@@ -1,1 +1,15 @@
 #include "utilidades.h"
+
+void imprimirArray(int const * array,int util)
+{
+  for(int i=0;i<util;i++)
+  {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+int mezclarUnico(int const * array1,int util1,int const * array2,int util2,int * array3)
+{
+
+}
