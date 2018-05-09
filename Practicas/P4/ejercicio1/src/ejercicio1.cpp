8389ae3e5c820d34da80e58becc4234ae5c934b7
xexugarcia95 <xexugarcia95@gmail.com>
2018-05-09 17:15:28 +0200

    ejercicio1, falta menú

--- Practicas/P4/ejercicio1/src/ejercicio1.cpp
+++ Practicas/P4/ejercicio1/src/ejercicio1.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main()
 {
-
+  cout << "\tBIENVENIDO AL JUEGO DE LOS BARQUITOS\n";
+  Barquitos *b = new Barquitos(11,11);
+  cout << "Primer barco\n";
+  b->mostrar();
+  Barquitos *c = new Barquitos(*b);
+  cout << "Segundo barco\n";
+  c->mostrar();
+  Barquitos *d = new Barquitos(5,5);
+  cout << "Tercer\n";
+  d->mostrar();
+  d=c;
+  cout << "Cuarto\n";
+  d->mostrar();
+  delete b;
+  delete d;
+  delete c;
 
   return 0;
 }
