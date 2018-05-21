3721b28d694bcf91478d3a08834f3c3d273afae9
xexugarcia95 <xexugarcia95@gmail.com>
2018-05-21 18:09:53 +0200

    .h

--- Practicas/P4/ejercicio2/include/BigInt.h
+++ Practicas/P4/ejercicio2/include/BigInt.h
@@ -7,12 +7,18 @@ using namespace std;
 
 class BIGINT
 {
-  
 private:
+
+  int *entero;
 
 public:
 
+BigInt();
+BigInt(const BigInt& b);
+~BigInt();
 
+int* sumaBig(int *b1,int *b2);
+ostream& operator<<(ostream& os,const BigInt& b);//friend ??
 
 };
 
