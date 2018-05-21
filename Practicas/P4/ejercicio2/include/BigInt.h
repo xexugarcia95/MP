b87e700a125c9159ca480a464e76e8e478b8faea
Jesus Garcia <xexugarcia95@gmail.com>
2018-05-21 22:52:33 +0200

    Implementacion casi terminada

--- Practicas/P4/ejercicio2/include/BigInt.h
+++ Practicas/P4/ejercicio2/include/BigInt.h
@@ -1,25 +1,35 @@
 #ifndef _BIGINT_H__
 #define _BIGINT_H__
-
+#include <string>
 #include <iostream>
 using namespace std;
 
 
-class BIGINT
+class BigInt
 {
 private:
 
   int *entero;
+  int elementoMaximo;
 
 public:
 
 BigInt();
+BigInt(int valor);
 BigInt(const BigInt& b);
 ~BigInt();
 
 int* sumaBig(int *b1,int *b2);
-ostream& operator<<(ostream& os,const BigInt& b);//friend ??
+
+friend ostream& operator<<(ostream& os,const BigInt& b);
 
 };
 
-#endif;
+ostream& operator<<(ostream& os,const BigInt& b)
+{
+  for(int i=b.elementoMaximo-1;i>=0;i--) os << b.entero[i];
+  os << endl;
+  return os;
+}
+
+#endif
--- Practicas/P4/ejercicio2/src/BigInt.cpp
+++ Practicas/P4/ejercicio2/src/BigInt.cpp
@@ -1,1 +1,45 @@
 #include "BigInt.h"
+#include <cstdlib>
+
+BigInt::BigInt()
+{
+  entero = 0;
+  elementoMaximo = 0;
+}
+
+BigInt::BigInt(int valor)
+{
+  int aux = valor;
+  int cont =1;
+  for(int i=valor;i!=0;i/=10) cont++;
+
+  char cadena[cont];
+  itoa(aux,cadena,10);
+  elementoMaximo = cont;
+  entero = new int[elementoMaximo];
+
+  for(int i=0;i<elementoMaximo;i++)
+  {
+    entero[i] = cadena[i];
+  }
+}
+
+BigInt::BigInt(const BigInt& b)
+{
+  elementoMaximo = b.elementoMaximo;
+  entero = new int[elementoMaximo];
+  for(int i=0;i<elementoMaximo;i++)
+  {
+    entero[i] = b.entero[i];
+  }
+}
+BigInt::~BigInt()
+{
+
+}
+
+int* BigInt::sumaBig(int *b1,int *b2)
+{
+
+
+}
