8e0e60ac7b768c88dbf5eafe274e27ef5fc14348
Jesus Garcia <xexugarcia95@gmail.com>
2018-05-21 22:52:40 +0200

    solucionar problema ostream

--- Practicas/P4/ejercicio2/src/ejercicio2.cpp
+++ Practicas/P4/ejercicio2/src/ejercicio2.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
+  cout << "EJERCICIO 2: TRATO DE VALORES BIG INT\n";
+  cout << "---------------------------------------\n";
+
+  int valor = 73759835;
+  BigInt *b = new BigInt;
 
 
   return 0;
