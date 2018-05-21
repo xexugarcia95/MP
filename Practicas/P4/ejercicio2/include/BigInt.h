95aff50a4a0e46c8700b9afbff8cf840842e246d
xexugarcia95 <xexugarcia95@gmail.com>
2018-05-21 17:47:50 +0200

    Creado archivos para Ejercicio2. Realizar implementación

---
+++ Practicas/P4/ejercicio2/include/BigInt.h
@@ -0,0 +1,19 @@
+#ifndef _BIGINT__
+#define _BIGINT__
+
+#include <iostream>
+using namespace std;
+
+
+class BIGINT
+{
+  
+private:
+
+public:
+
+
+
+};
+
+#endif;
---
+++ Practicas/P4/ejercicio2/src/ejercicio2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "BigInt.h"
+
+using namespace std;
+
+int main()
+{
+
+
+  return 0;
+}
