db184c4e6fd2bdb613ece4fe73b2e363a776bb74
Jesus Garcia <xexugarcia95@gmail.com>
2018-05-27 23:47:19 +0200

    .

--- Practicas/P4/ejercicio5/include/MatrizDispersa.h
+++ Practicas/P4/ejercicio5/include/MatrizDispersa.h
@@ -8,10 +8,28 @@ using namespace std;
 class MatrizDispersa
 {
 private:
-
+  int nfilas;
+  int ncolumnas;
+  Valor *valores;
+  int numeroValores;
 public:
+  MatrizDispersa();
+  MatrizDispersa(int n,int c,Valor *v,int num);
+  MatrizDispersa(const MatrizDispersa& m);
+  ~MatrizDispersa();
 
+  MatrizDispersa* operator+(const MatrizDispersa& m);
+  friend ostream& operator<<(ostream& os,const MatrizDispersa& m);
 
 };
+
+inline ostream& operator<<(ostream& os,const MatrizDispersa& m)
+{
+  os << "Filas: " << m.nfilas << ", Columnas: " << m.ncolumnas << endl;
+  os << "Valores: " << m.valores << endl;
+  os << "Numero de Valores: " << m.numeroValores << endl;
+
+  return os;
+}
 
 #endif
--- Practicas/P4/ejercicio5/src/MatrizDispersa.cpp
+++ Practicas/P4/ejercicio5/src/MatrizDispersa.cpp
@@ -1,1 +1,32 @@
 #include "MatrizDispersa.h"
+
+MatrizDispersa::Matriz()
+{
+  this->nfilas = 0;
+  this->ncolumnas = 0;
+  this->valores = 0;
+  this->numeroValores = 0;
+}
+
+MatrizDispersa::MatrizDispersa(int n,int c,Valor *v,int num)
+{
+  this->nfilas = n;
+  this->ncolumnas = c;
+  this->numeroValores = num;
+  this->valores = v;
+}
+
+MatrizDispersa::MatrizDispersa(const MatrizDispersa& m)
+{
+
+}
+
+MatrizDispersa::~MatrizDispersa()
+{
+
+}
+
+MastMatrizDispersa::* operator+(const MatrizDispersa& m)
+{
+
+}
